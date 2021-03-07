6e9dafc0695f81d8f6cb54881829d444870d0f4f
sudhirdontha72 <sudhirdontha@outlook.com>
2021-03-07 15:28:19 +0530

    Added server.js
    Added .gitignore to exclude the node_modules

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const jsonServer = require('json-server'); // get the package that you've installed 
+
+const server = jsonServer.create();
+
+const router = jsonServer.router('db.json');
+
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
